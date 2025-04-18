393b3584970ba503dc48a5bca0d5eab2cee008c2
kewalaka <kewalaka@gmail.com>
2025-04-18 12:44:51 +1200

    feat: add Make target to test if changes are non-destructive

---
+++ avm_scripts/test-non-destructive.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+export TF_VAR_msi_id=$MSI_ID
+
+# if $AVM_MOD_PATH and $AVM_EXAMPLE are not set, exit
+if [ -z "$AVM_MOD_PATH" ] || [ -z "$AVM_EXAMPLE" ]; then
+    echo "AVM_MOD_PATH and AVM_EXAMPLE must be set"
+    exit 1
+fi
+
+# change dir into $AVM_MOD_PATH/examples/$AVM_EXAMPLE
+# then run pre.sh if it exists
+cd "$AVM_MOD_PATH/examples/$AVM_EXAMPLE"
+if [ -f pre.sh ]; then
+    echo "==> Running pre.sh"
+    chmod +x pre.sh
+    ./pre.sh
+fi
+
+cd /tmp
+testFolder="avmtester$AVM_EXAMPLE$RANDOM"
+
+git clone -b feat/upgrade-testing https://github.com/kewalaka/avmtester.git $testFolder && cd $testFolder
+go mod tidy
+if [ -z "${TEST_TIMEOUT}" ]; then
+    export TEST_TIMEOUT=720m
+fi
+echo "==> go test"
+echo $TEST_TIMEOUT
+terraform version
+test_failed=false
+go test -v -timeout=$TEST_TIMEOUT -run TestDestructiveUpgradeExample || test_failed=true
+
+# change dir back into the example and run post.sh if it exists
+cd "$AVM_MOD_PATH/examples/$AVM_EXAMPLE"
+if [ -f post.sh ]; then
+    echo "==> Running post.sh"
+    chmod +x ./post.sh
+    ./post.sh
+fi
+
+if [ "$test_failed" = true ]; then
+    echo "==> Test failed"
+    exit 1
+fi
